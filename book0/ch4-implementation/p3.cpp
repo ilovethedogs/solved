21735918a82bc59342ee25b3ef255c261402d6ea
ilovethedogs <79030282+ilovethedogs@users.noreply.github.com>
2022-06-28 18:17:36 +0900

    Update p3.cpp

--- book0/ch4-implementation/p3.cpp
+++ book0/ch4-implementation/p3.cpp
@@ -3,51 +3,66 @@
 #include <vector>
 #include <array>
 
-int main() {
-    int n, m {};
+int main()
+{
+    int n, m{};
     std::cin >> n >> m;
 
-    std::pair<int, int> initPos {};
-    int curDir {};
+    std::pair<int, int> initPos{};
+    int curDir{};
     std::cin >> initPos.first >> initPos.second >> curDir;
 
-    std::cin.get();
-    char map[2 * n * m + 1] {};
+    char map[2 * n * m + 1]{};
+    char lf[1]{};
+    std::cin.getline(lf, 1);
     std::cin.getline(map, 2 * n * m, '#');
-    
-    int result {1};
+
+    int delta[4] {-2 * n, 2, 2 * n, -2};
+    int goback[4] {2, 2, -2, -2};
+
+    int result{1};
     int cur = initPos.first * 2 * n + initPos.second * 2;
     map[cur] = '2';
     while (true) {
-        keepgoing:
-        for (int dir {curDir}; dir % 4 != curDir; ++dir) {
-            int dirPos {cur + (-2 * n) * (dir == 0) + 2 * (dir == 1) + (2 * n) * (dir == 2) + (-2) * (dir == 3)};
-            switch (dirPos) {
-                case '1':
-                case '2':
-                    continue;
-                case '0':
-                    curDir = dir;
-                    cur = dirPos;
-                    ++result;
-                    map[cur] = '2';
-                    goto keepgoing;
+    keepgoing:
+        for (int dir{(curDir + 1) % 4}; dir % 4 != curDir; ++dir)
+        {
+            int dirPos{cur + delta[dir]};
+            switch (map[dirPos])
+            {
+            case '1':
+            case '2':
+                continue;
+            case '0':
+                curDir = dir;
+                cur = dirPos;
+                ++result;
+                map[cur] = '2';
+                goto keepgoing;
             }
         }
         // go back
-        int backPos {cur + (-2 * n) * (curDir == 0) + 2 * (curDir == 1) + (2 * n) * (curDir == 2) + (-2) * (curDir == 3)};
-        if (backPos < 0 || backPos >= 2 * n * m) {
-            break;        
+        int backPos{cur + delta[curDir]};
+        if (backPos < 0 || backPos >= 2 * n * m)
+        {
+            break;
         }
 
-        switch (map[backPos]) {
-            case 1: goto done;
-            case 0: ++result; map[backPos] = '2';
-            case 2: cur = backPos; curDir += 2 * (curDir == 0 || curDir == 1) - 2 * (curDir == 2 || curDir == 3); goto keepgoing;
+        switch (map[backPos])
+        {
+        case '1':
+            goto done;
+        case '0':
+            ++result;
+            map[backPos] = '2';
+        case '2':
+            cur = backPos;
+            curDir += goback[curDir];
+            goto keepgoing;
         }
     }
-       
-    done:
+
+done:
     std::cout << result << std::endl;
 
     return 0;
