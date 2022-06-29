09da687745fec46b72b669ea1b9196ba993edc86
ilovethedogs <79030282+ilovethedogs@users.noreply.github.com>
2022-06-29 14:09:00 +0900

    done ch5

---
+++ book0/ch5-dfs-bfs/ch5-p3.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <utility>
+
+int main() {
+    int n, m {};
+    std::cin >> n >> m;
+
+    char lf[1] {};
+    std::cin.getline(lf, 1);
+    char ice_str[n * (m + 1) + 1];
+    std::cin.getline(ice_str, n * (m + 1), '#');
+
+    int ice[n][m] {};
+    for (auto row {0}, idx {0}; row != n; ++row) {
+        for (auto col {0}; col != m; ++col) {
+            ice[row][col] = static_cast<int>(ice_str[idx] - '0');
+            ++idx;
+        }
+        ++idx;
+    }
+
+    std::stack<std::pair<int, int>> s {};
+    int result {0};
+    for (auto row {0}; row != n; ++row) {
+        for (auto col {0}; col != m; ++col) {
+            if (ice[row][col] == 0) {
+                ++result;
+
+                s.emplace(std::pair<int, int> {row, col});
+                while (!s.empty()) {
+                    std::pair<int, int> cur {s.top()};
+                    s.pop();
+                    ice[cur.first][cur.second] = 2;
+
+                    if (cur.first - 1 > 0 && ice[cur.first - 1][cur.second] == 0)
+                        s.emplace(std::pair<int, int> {cur.first - 1, cur.second});
+                    if (cur.second + 1 < m && ice[cur.first][cur.second + 1] == 0)
+                        s.emplace(std::pair<int, int> {cur.first, cur.second + 1});
+                    if (cur.first + 1 < n && ice[cur.first + 1][cur.second] == 0)
+                        s.emplace(std::pair<int, int> {cur.first + 1, cur.second});
+                    if (cur.second - 1 > 0 && ice[cur.first][cur.second - 1] == 0)
+                        s.emplace(std::pair<int, int> {cur.first, cur.second - 1});
+                }
+            }
+        }
+    }
+
+    std::cout << result << std::endl;
+
+    return 0;
+}
---
+++ book0/ch5-dfs-bfs/ch5-p4.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+
+int main() {
+    int n, m {};
+    std::cin >> n >> m;
+    char lf[1] {};
+    std::cin.getline(lf, 1);
+
+    char map_str[n * (m + 1) + 1] {};
+    std::cin.getline(map_str, n * (m + 1), '#');
+
+    int map[n][m] {};
+    for (auto row {0}, idx {0}; row != n; ++row) {
+        for (auto col {0}; col != m; ++col) {
+            map[row][col] = static_cast<int>(map_str[idx] - '0');
+            ++idx;
+        }
+        ++idx;
+    }
+
+    std::queue<std::pair<int, int>> q {};
+    q.emplace(std::pair<int, int> {0, 0});
+    while (!q.empty()) {
+        std::pair<int, int> cur {q.front()};
+        q.pop();
+
+        if (cur.first == n - 1 && cur.second == m - 1) break;
+
+        auto cnt {0};
+        if (cur.first - 1 > 0 && map[cur.first - 1][cur.second] == 1) {
+            q.emplace(std::pair<int, int> {cur.first - 1, cur.second});
+            map[cur.first - 1][cur.second] = map[cur.first][cur.second] + 1;
+        }
+        if (cur.second - 1 > 0 && map[cur.first][cur.second - 1] == 1) {
+            q.emplace(std::pair<int, int> {cur.first, cur.second - 1});
+            map[cur.first][cur.second - 1] = map[cur.first][cur.second] + 1;
+        }
+        if (cur.first + 1 < n && map[cur.first + 1][cur.second] == 1) {
+            q.emplace(std::pair<int, int> {cur.first + 1, cur.second});
+            map[cur.first + 1][cur.second] = map[cur.first][cur.second] + 1;
+        }
+        if (cur.second + 1 < m && map[cur.first][cur.second + 1] == 1) {
+            q.emplace(std::pair<int, int> {cur.first, cur.second + 1});
+            map[cur.first][cur.second + 1] = map[cur.first][cur.second] + 1;
+        }
+    }
+
+    std::cout << std::endl;
+    for (int row {0}; row != n; ++row) {
+        for (int col {0}; col != m; ++col) {
+            std::cout << map[row][col];
+        }
+        std::cout << std::endl;
+    }
+    std::cout << map[n-1][m-1] << std::endl;
+
+    return 0;
+}
